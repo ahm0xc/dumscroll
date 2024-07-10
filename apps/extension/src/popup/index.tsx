633e0801f39b670055fe154a02e4320ea059d141
ahm0xc <codingwebsa@gmail.com>
2024-07-10 23:06:06 +0600

    clean

--- apps/extension/src/popup/index.tsx
+++ apps/extension/src/popup/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Popup } from "./Popup";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("app") as HTMLElement).render(
   <React.StrictMode>
-    <Popup />
+    <Hello />
   </React.StrictMode>,
 );
+
+function Hello() {
+  return "Hello";
+}
