86512f8cb90efec22d782f9186b57ad69ec4bced
ahm0xc <codingwebsa@gmail.com>
2024-07-11 23:06:35 +0600

    test simple charts

--- apps/www/src/app/(dashboard)/dashboard/nav.tsx
+++ apps/www/src/app/(dashboard)/dashboard/nav.tsx
@@ -4,27 +4,15 @@ import { cn } from "~/lib/utils";
 
 export default function Nav({ className, ...props }: React.HTMLAttributes<HTMLElement>) {
   return (
-    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)} {...props}>
+    <nav className={cn("flex items-center space-x-4 lg:space-x-5", className)} {...props}>
       <Link href="/dashboard" className="text-sm font-medium transition-colors hover:text-primary">
-        Overview
+        Dashboard
       </Link>
       <Link
-        href="/dashboard"
+        href="/knowledge"
         className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
       >
-        Customers
-      </Link>
-      <Link
-        href="/dashboard"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Products
-      </Link>
-      <Link
-        href="/dashboard"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Settings
+        Knowledge
       </Link>
     </nav>
   );
--- apps/www/src/app/(dashboard)/dashboard/page.tsx
+++ apps/www/src/app/(dashboard)/dashboard/page.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from "next";
 import Link from "next/link";
 
 import Logo from "~/components/logo";
-import { Button } from "~/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import Nav from "./nav";
 import UserNav from "./user-nav";
 import ThemeToggle from "./theme-toggle";
+import UsesChart from "./uses-chart";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -16,11 +16,12 @@ export const metadata: Metadata = {
 export default function DashboardPage() {
   return (
     <main>
-      <div className="hidden flex-col md:flex">
+      <div className="flex-col flex">
         <div className="border-b">
           <div className="flex h-16 items-center px-4">
-            <Link href="/dashboard">
+            <Link href="/dashboard" className="flex items-center gap-2">
               <Logo className="h-6 w-6 invert dark:invert-0" />
+              <p className="text-lg tracking-tight font-semibold hidden md:block">Dumscroll</p>
             </Link>
             <Nav className="mx-6" />
             <div className="ml-auto flex items-center space-x-4">
@@ -36,17 +37,16 @@ export default function DashboardPage() {
           <Tabs defaultValue="overview" className="space-y-4">
             <TabsList>
               <TabsTrigger value="overview">Overview</TabsTrigger>
-              <TabsTrigger value="analytics">Analytics</TabsTrigger>
+              <TabsTrigger value="analytics">Uses</TabsTrigger>
               <TabsTrigger value="reports">Reports</TabsTrigger>
-              <TabsTrigger value="notifications" disabled>
-                Notifications
-              </TabsTrigger>
             </TabsList>
             <TabsContent value="overview" className="space-y-4">
-              overview
+              <div>Overview</div>
             </TabsContent>
             <TabsContent value="analytics" className="space-y-4">
-              analytics
+              <div>
+                <UsesChart />
+              </div>
             </TabsContent>
             <TabsContent value="reports" className="space-y-4">
               reports
