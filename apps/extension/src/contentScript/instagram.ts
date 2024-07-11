0a35913da4c98d1bd1ea266810db00044ed0448a
ahm0xc <codingwebsa@gmail.com>
2024-07-11 11:05:41 +0600

    lock reel scroll

--- apps/extension/src/contentScript/instagram.ts
+++ apps/extension/src/contentScript/instagram.ts
@@ -1,1 +1,17 @@
-import "./instagram.css"
+import detectUrlChange from "detect-url-change";
+import "./instagram.css";
+
+function main() {
+  detectUrlChange.on("change", (newUrl) => {
+    if (newUrl.includes("/reels/")) {
+      const url = new URL(newUrl);
+      if (url.pathname.includes("/reels/")) {
+        document.body.setAttribute("data-lock-scroll", "true");
+      } else {
+        document.body.setAttribute("data-lock-scroll", "false");
+      }
+    }
+  });
+}
+
+main();
