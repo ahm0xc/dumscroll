0d777abb2c38acb23fa993d7ed750b13767c6713
ahm0xc <codingwebsa@gmail.com>
2024-07-11 15:40:47 +0600

    simple dashboard page

---
+++ apps/www/src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import type { Metadata } from "next";
+
+import { Button } from "~/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import Logo from "~/components/logo";
+import UserNav from "./user-nav";
+import Nav from "./nav";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "",
+};
+
+export default function DashboardPage() {
+  return (
+    <main>
+      <div className="hidden flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <Link href="/dashboard">
+              <Logo className="h-6 w-6 invert" />
+            </Link>
+            <Nav className="mx-6" />
+            <div className="ml-auto flex items-center space-x-4">
+              <UserNav />
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <div className="flex items-center justify-between space-y-2">
+            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+            <div className="flex items-center space-x-2">
+              <Button>Download</Button>
+            </div>
+          </div>
+          <Tabs defaultValue="overview" className="space-y-4">
+            <TabsList>
+              <TabsTrigger value="overview">Overview</TabsTrigger>
+              <TabsTrigger value="analytics">Analytics</TabsTrigger>
+              <TabsTrigger value="reports">Reports</TabsTrigger>
+              <TabsTrigger value="notifications" disabled>
+                Notifications
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="overview" className="space-y-4">
+              overview
+            </TabsContent>
+            <TabsContent value="analytics" className="space-y-4">
+              analytics
+            </TabsContent>
+            <TabsContent value="reports" className="space-y-4">
+              reports
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </main>
+  );
+}
--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Button } from '~/components/ui/button'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <Button>Hello</Button>
+    </div>
   )
 }
