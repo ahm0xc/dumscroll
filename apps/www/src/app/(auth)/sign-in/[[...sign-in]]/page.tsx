10ccedd15feb353b2a1eeb98101866636cb31215
ahm0xc <codingwebsa@gmail.com>
2024-07-13 12:24:58 +0600

    redirect to get started page after signing in

--- apps/www/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ apps/www/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="grid place-content-center h-screen">
-      <SignIn fallbackRedirectUrl="/dashboard" />
+      <SignIn fallbackRedirectUrl="/dashboard/get-started" />
     </div>
   );
 }
--- apps/www/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ apps/www/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="grid h-screen place-content-center">
-      <SignUp fallbackRedirectUrl="/dashboard" />
+      <SignUp fallbackRedirectUrl="/dashboard/get-started" />
     </div>
   );
 }
