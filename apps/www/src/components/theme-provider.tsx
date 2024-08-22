7dcb97275f2ccc3661cf84f22dacb3dfc92f9cf7
ahm0xc <codingwebsa@gmail.com>
2024-08-22 21:16:08 +0600

    make light mode as default theme

--- apps/www/src/components/theme-provider.tsx
+++ apps/www/src/components/theme-provider.tsx
@@ -8,7 +8,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange
       {...props}
