c65888feb085db464601ace7a72f9c4fe02a0bf5
ahm0xc <codingwebsa@gmail.com>
2024-08-22 20:20:18 +0600

    fix daily time

--- apps/extension/src/options/analytics/daily-screen-time.tsx
+++ apps/extension/src/options/analytics/daily-screen-time.tsx
@@ -7,7 +7,9 @@ export default function DailyScreenTime({ tracks }: { tracks: Track[] }) {
   const today = new Date();
   const dateId = `${today.getFullYear()}-${(today.getMonth() + 1).toString().padStart(2, "0")}-${today.getDate().toString().padStart(2, "0")}`;
 
-  const todaysTracks = tracks.filter((t) => getDateFromTrackId(t.id) === dateId);
+  const todaysTracks = tracks.filter(
+    (t) => getDateFromTrackId(t.id) === dateId,
+  );
 
   const screenTime = todaysTracks.reduce((acc, currentTrack) => {
     return acc + currentTrack.duration;
@@ -23,7 +25,7 @@ export default function DailyScreenTime({ tracks }: { tracks: Track[] }) {
         <p className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
           {screenTimeMinutes < 60
             ? `${screenTimeMinutes} min`
-            : `${Math.floor(screenTimeMinutes / 60)} hours`}
+            : `${Math.floor(screenTimeMinutes / 60)} hours ${Math.floor(screenTimeMinutes / 60) % 60} min`}
         </p>
         <p className="mt-4 flex flex-col gap-1 text-tremor-default text-tremor-content dark:text-dark-tremor-content">
           <span>The amount of time you spent today on your browser.</span>
