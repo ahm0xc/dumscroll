d88b594416d550413dd75df0edd1d018451fd348
ahm0xc <codingwebsa@gmail.com>
2024-09-08 13:27:56 +0600

    remove dashboard

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -6,6 +6,12 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async headers() {
     return [
       {
