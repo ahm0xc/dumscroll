f1a43f9b9ad22c9be0c0df040c5e714cbb00abf9
ahm0xc <codingwebsa@gmail.com>
2025-04-12 21:50:38 +0600

    update card shadow

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow",
+      "rounded-xl border bg-card text-card-foreground shadow-[0_10px_10px_-5px_rgba(0,0,0,0.05)]",
       className,
     )}
     {...props}
